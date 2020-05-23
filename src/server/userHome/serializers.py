7ca617702f924a19263103c60e8e171f7398b173
ironwall24 <gabosorio_3808@hotmail.com>
2020-05-23 19:52:43 -0400

    Fix(UserHomeSerializer): UserData field removed from the model

--- src/server/userHome/serializers.py
+++ src/server/userHome/serializers.py
@@ -10,5 +10,4 @@ class UserHomeSerializer(serializers.ModelSerializer):
             "residenceName",
             "streetBlockNumber",
             "homeNumber",
-            "usersdata",
         ]
