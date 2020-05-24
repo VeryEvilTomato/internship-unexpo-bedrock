9b4cb318aa149c5eeed7628d8bc7d666f1376166
ironwall24 <gabosorio_3808@hotmail.com>
2020-05-24 13:49:50 -0400

    Feat(UserSerializer): new feature added for authentication data validation.

--- src/server/userData/serializers.py
+++ src/server/userData/serializers.py
@@ -1,12 +1,24 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from userData.models import UserData
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+        )
+        return user
     # Model User fields that get serialized
+
     class Meta:
-        model = User
+        model = get_user_model()
         fields = [
             "id",
             "username",
